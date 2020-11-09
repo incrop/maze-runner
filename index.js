a647bd49b9aba98c50befc40781c51316fb92bf8
Dmitry Starostin <incrop@gmail.com>
2020-11-10 00:04:11 +0300

    Add skeletons

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const params = new URLSearchParams(window.location.search);
 const WIDTH = parseInt(params.get('w'), 10) || 13;
 const HEIGHT = parseInt(params.get('h'), 10) || 6;
 const TREASURES = parseInt(params.get('t'), 10) || 8;
+const SKELETONS = parseInt(params.get('s'), 10) || 2;
 const EDIT_MODE = !!params.get('e');
 
 const SPRITES = {
@@ -24,11 +25,16 @@ SPRITES.ArrowLeft = loadSprites('img/left-stand.svg', 'img/left-move1.svg', 'img
 SPRITES.ArrowUp = loadSprites('img/up-stand.svg', 'img/up-move1.svg', 'img/up-move2.svg');
 SPRITES.ArrowRight = loadSprites('img/right-stand.svg', 'img/right-move1.svg', 'img/right-move2.svg', 'img/right-move3.svg', 'img/right-move2.svg');
 SPRITES.treasure = loadSprites('img/chest.svg', 'img/gem1.svg', 'img/gem2.svg', 'img/gem3.svg');
+SPRITES.skeleton = loadSprites('img/skeleton1.svg', 'img/skeleton2.svg', 'img/skeleton3.svg', 'img/skeleton4.svg');
 SPRITES.win = loadSprites('img/down-stand.svg', 'img/down-cheer.svg');
 
 const SOUND = {
     background: new Audio('sound/background.ogg'),
     treasure: new Audio('sound/treasure.ogg'),
+    skeleton: {
+        wake: new Audio('sound/skeleton-wake.ogg'),
+        dead: new Audio('sound/skeleton-dead.ogg'),
+    },
     win: new Audio('sound/win.ogg'),
 };
 
@@ -104,6 +110,32 @@ class Treasure {
     }
 }
 
+class Skeleton {
+    constructor() {
+        this.state = 'sleep';
+        this.progress = false;
+    }
+    draw(timestamp, ctx, x, y) {
+        const sprite = SPRITES.skeleton[this._spriteIdx(timestamp)];
+        const center = Math.floor(SETTINGS.cellSize / 2);
+        const h = Math.floor(sprite.height);
+        const w = Math.floor(sprite.width);
+        x += center - Math.floor(w / 2);
+        y += center - Math.floor(h / 2);
+        ctx.drawImage(sprite, x, y, w, h);
+    }
+    _spriteIdx(timestamp) {
+        switch (this.state) {
+            case 'sleep':
+                return 0;
+            case 'wake':
+                return this.progress ? 1 : 2;
+            case 'dead':
+                return this.progress ? 1 : 3;               
+        }
+    }
+}
+
 class Maze {
     constructor(height, width) { 
         this.height = height; 
@@ -112,6 +144,7 @@ class Maze {
         this.verWalls = Array.from(Array(height), () => Array(width + 1));
         this.items = Array.from(Array(height), () => Array(width));
         this.player = new Player(0, 0);
+        this.skeletons = [];
         this.treasures = 0;
         this.changedCells = [];
         this.init();
@@ -125,7 +158,6 @@ class Maze {
             this.verWalls[i][0] = true;
             this.verWalls[i][this.width] = true;
         }
-        this.items[0][0] = this.player;
     }
     canMove(direction) {
         const p = this.player;
@@ -157,12 +189,13 @@ class Maze {
         if (!p.moveTimestamp) {
             if (this.tryMoveAny(directions)) {
                 p.moveTimestamp = timestamp;
+            } else {
+                return;
             }
-            return;
         }
-        this.changedCells.unshift(p.targetCell());
+        this.changedCells.push(p.targetCell());
+        this._checkIfSkeletonNearby(timestamp);
         if (timestamp - p.moveTimestamp > SETTINGS.moveTimeMs) {
-            this.items[p.i][p.j] = null;
             [p.i, p.j] = p.targetCell();
             if (this.items[p.i][p.j] instanceof Treasure) {
                 if (--this.treasures === 0) {
@@ -176,8 +209,11 @@ class Maze {
                         SOUND.treasure.fastSeek(0);
                     }
                 }
+                this.items[p.i][p.j] = null;
             }
-            this.items[p.i][p.j] = p;
+            for (const skeleton of this.skeletons) {
+                skeleton.progress = false;
+            }
             if (p.winTimestamp) {
                 p.winTimestamp = p.moveTimestamp + SETTINGS.moveTimeMs;
             }
@@ -185,6 +221,39 @@ class Maze {
                 p.moveTimestamp += SETTINGS.moveTimeMs;
             } else {
                 p.moveTimestamp = null;
+            }
+        }
+    }
+    _checkIfSkeletonNearby(timestamp) {
+        const [i1, j1] = this.player.targetCell();
+        if (this.items[i1][j1] instanceof Skeleton) {
+            const skeleton = this.items[i1][j1];
+            if (skeleton.state === 'wake') {
+                skeleton.state = 'dead';
+                skeleton.progress = true;
+                SOUND.skeleton.dead.play();
+            }
+            if (skeleton.progress) {
+                this.changedCells.push([i1, j1]);
+            }    
+            return;
+        }
+        for (const [di, dj] of [[1, 0], [0, 1], [-1, 0], [0, -1]]) {
+            const [i2, j2] = [i1 + di, j1 + dj];
+            if (!this.items[i2] || !this.items[i2][j2]) {
+                continue;
+            }
+            if (this.items[i2][j2] instanceof Skeleton) {
+                const skeleton = this.items[i2][j2];
+                if (skeleton.state === 'sleep') {
+                    skeleton.state = 'wake';
+                    skeleton.progress = true;
+                    SOUND.skeleton.wake.play();
+                }
+                if (skeleton.progress) {
+                    this.changedCells.push([i2, j2]);
+                }
+                return;
             }
         }
     }
@@ -216,6 +285,7 @@ class Viewport {
                 this._drawItem(timestamp, ctx, i, j);
             }
         }
+        this._drawPlayer(timestamp, ctx);
     }
     drawChanged(timestamp) {
         const ctx = this.canvas.getContext('2d');
@@ -231,6 +301,7 @@ class Viewport {
         for (const [i, j] of this.maze.changedCells) {
             this._drawItem(timestamp, ctx, i, j);
         }
+        this._drawPlayer(timestamp, ctx);
     }
     _clearCell(timestamp, ctx, i, j) {
         const y = this.offsetY + i * SETTINGS.cellSize + SETTINGS.wallWidth;
@@ -256,14 +327,21 @@ class Viewport {
         }
     
     }
-    _drawItem(timestamp, ctx, i, j) {
+    _drawItem(timestamp, ctx, i, j, Clazz) {
         const items = this.maze.items;
         if (!items[i] || !items[i][j]) {
             return;
         }
         const y = this.offsetY + i * SETTINGS.cellSize;
         const x = this.offsetX + j * SETTINGS.cellSize;
+        const p = this.maze.player;
         items[i][j].draw(timestamp, ctx, x, y);
+    }
+    _drawPlayer(timestamp, ctx) {
+        const p = this.maze.player;
+        const y = this.offsetY + p.i * SETTINGS.cellSize;
+        const x = this.offsetX + p.j * SETTINGS.cellSize;
+        p.draw(timestamp, ctx, x, y);
     }
 }
 
@@ -281,13 +359,12 @@ function generateDFS(maze) {
 
     maze.treasures = TREASURES;
     for (let d = 0; d < TREASURES; d++) {
-        let i;
-        let j;
-        do {
-            i = Math.floor(Math.random() * h);
-            j = Math.floor(Math.random() * w);
-        } while (maze.items[i][j]);
-        maze.items[i][j] = new Treasure();
+        putAtRandom(new Treasure());
+    }
+    for (let s = 0; s < SKELETONS; s++) {
+        const skeleton = new Skeleton();
+        maze.skeletons.push(skeleton);
+        putAtRandom(skeleton);
     }
 
     generateStep(Math.floor(Math.random() * h), Math.floor(Math.random() * w));
@@ -323,6 +400,16 @@ function generateDFS(maze) {
 
             }
         }
+    }
+
+    function putAtRandom(item) {
+        let i;
+        let j;
+        do {
+            i = Math.floor(Math.random() * h);
+            j = Math.floor(Math.random() * w);
+        } while ((i + j) <= 1 || maze.items[i][j]);
+        maze.items[i][j] = item;
     }
 
     function shuffleDir() {
@@ -432,10 +519,11 @@ function play() {
         const dx = x - (view.offsetX + j * SETTINGS.cellSize + SETTINGS.cellSize / 2);
         const dy = y - (view.offsetY + i * SETTINGS.cellSize + SETTINGS.cellSize / 2);
         if (Math.abs(dx) < SETTINGS.cellSize / 4 && Math.abs(dy) < SETTINGS.cellSize / 4) {
-            const item = maze.items[i][j];
-            if (item instanceof Player) {
+            const p = maze.player;
+            if (p.i === i && p.j === j) {
                 return;
             }
+            const item = maze.items[i][j];
             maze.items[i][j] = item ? null : new Treasure();
             requestAnimationFrame(redraw);
             return;
